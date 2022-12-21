965da5e0361fcaaa5b9e7694b3267cb026b2b6f5
Manuel Da Pena <65864237+NachoDPP@users.noreply.github.com>
2022-12-21 13:33:44 -0400

    Controllers Scaffolding

---
+++ controllers/album_controller.py
@@ -0,0 +1,9 @@
+from fastapi import APIRouter, Body, Depends, HTTPException, Path, status
+from sqlalchemy.orm import Session
+
+from dependencies import get_blog_repo, get_db
+
+router = APIRouter(
+    prefix="/albums",
+    tags=["Albums"]
+)
