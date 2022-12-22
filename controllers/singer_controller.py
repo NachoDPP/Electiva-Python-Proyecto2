1dc97b8aee470c0d2561f76e1323665865393387
Manuel Da Pena <65864237+NachoDPP@users.noreply.github.com>
2022-12-21 20:04:04 -0400

    Controller Implementation

--- controllers/singer_controller.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter, Body, Depends, HTTPException, Path, status
-from sqlalchemy.orm import Session
-
-from dependencies import get_blog_repo, get_db
-
-router = APIRouter(
-    prefix="/singers",
-    tags=["Singers"]
-)
--- main.py
+++ main.py
@@ -2,14 +2,18 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from config_db import Base, engine
+from controllers.music_controller import router as music_router
 
 def get_application():
+    """Instancia una nueva aplicación de FastAPI pre-configurada"""
 
-    # crea la base de datos
+    # Creamos la base de datos
     Base.metadata.create_all(bind=engine)
 
-    app = FastAPI(title="API Blog demo", version="1.0.0")
+    # Creamos la aplicación de fastAPI
+    app = FastAPI(title="Proyecto 2 - Music Store", version="1.0.0", description="Manuel Da Pena")
 
+    # Habilitamos CORS
     app.add_middleware(
         CORSMiddleware,
         allow_origins=["*"],
@@ -17,6 +21,9 @@ def get_application():
         allow_methods=["*"],
         allow_headers=["*"],
     )
+
+    # Incluimos los Routers
+    app.include_router(music_router, prefix="/music-store/api/v1")
 
     return app
 
